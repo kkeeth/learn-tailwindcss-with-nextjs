d56a65ca260a7a4f398a0732ea960519f122c4ca
kkeeth <zensin0082@gmail.com>
2021-06-17 22:49:16 +0900

    feat: another card component

--- pages/learn.js
+++ pages/learn.js
@@ -8,7 +8,7 @@ export default function Learn() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-gray-100">
         <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
           <div class="flex-shrink-0">
             <img class="h-20 w-24" src="https://placedog.net/500/280" alt="random dog photo" />
@@ -16,6 +16,20 @@ export default function Learn() {
           <div>
             <div class="text-xl font-medium text-black">ChitChat</div>
             <p class="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
+        <div class="mt-10 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+          <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="https://placedog.net/500/400" alt="random dog photo" />
+          <div class="text-center space-y-2 sm:text-left">
+            <div class="space-y-0.5">
+              <p class="text-lg text-black font-semibold">
+                Erin Lindford
+              </p>
+              <p class="text-gray-500 font-medium">
+                Product Engineer
+              </p>
+            </div>
+            <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
           </div>
         </div>
       </main>
