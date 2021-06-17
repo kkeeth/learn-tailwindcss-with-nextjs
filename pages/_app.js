9c01a5c2ba84903617edd943870a5fead108f456
kkeeth <zensin0082@gmail.com>
2021-06-17 21:45:53 +0900

    feat: added and comment out if I use custom CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+// If you aren’t planning to write any custom CSS in your project
 import 'tailwindcss/tailwind.css'
+// use any custom CSS
+// import '../styles/gloals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
